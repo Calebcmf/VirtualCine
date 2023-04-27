c9a268ca6dbe986f5ba35a22f2ff541b3c68a350
Calebcmf <caleb_freitas@estudante.sesisenai.org.br>
2023-04-27 12:04:33 -0300

    novo

---
+++ inserir.php
@@ -0,0 +1,15 @@
+<?php 
+    include("conecta.php");
+
+    $nome = $_POST["nome"];
+    $cpf = $_POST["cpf"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    $comando = $pdo->prepare("INSERT INTO usuario(nome,cpf,email,senha) VALUES ('$nome', '$cpf','$email', '$senha')");
+
+    $resultado = $comando->execute();
+
+    //para voltar no fomulário:
+    header("Location: cartão.html");
+?>
