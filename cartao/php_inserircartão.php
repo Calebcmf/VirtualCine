43d3411abe82d537795e8945949227399ff05896
Calebcmf <caleb_freitas@estudante.sesisenai.org.br>
2023-06-15 12:13:39 -0300

    push

--- cartao/php_inserircartão.php
+++ cartao/php_inserircartão.php
@@ -5,8 +5,11 @@
     $numero = $_POST["num_cartao"];
     $cvv = $_POST["cvv"];
     $valid = $_POST["valid"];
+    $cartao = $_POST["cartao"];
+    $plano = $_POST["plano"];
 
-    $comando = $pdo->prepare("INSERT INTO cartao(titular,num_cartao,cvv,validade) VALUES ('$titular', '$numero','$cvv', '$valid')");
+
+    $comando = $pdo->prepare("INSERT INTO cartao(titular,num_cartao,cvv,validade,cartao,plano) VALUES ('$titular', '$numero','$cvv', '$valid', '$cartao', '$plano')");
 
     $resultado = $comando->execute();
  
