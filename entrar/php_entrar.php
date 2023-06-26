bac89ccfa98a568702d985b048cadc3af00fd714
Calebcmf <caleb_freitas@estudante.sesisenai.org.br>
2023-06-26 12:19:39 -0300

    push

---
+++ entrar/php_entrar.php
@@ -0,0 +1,22 @@
+<?php 
+
+    include("../php_conecta.php");
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // Consulta para verificar se o email e senha estão corretos
+    $comando = $pdo->prepare("SELECT * FROM usuario WHERE email = '$email' and senha = '$senha'");
+    $resultado = $comando->execute();
+
+    $n = $comando->rowCount();
+    if ($n > 0) {
+        // Email e senha corretos
+        header("Location: ../filmes/tela_filmes.html");
+    } else {
+        // Email ou senha incorretos
+        echo "Email ou senha incorretos. Tente novamente!";
+    }
+
+   
+?>
